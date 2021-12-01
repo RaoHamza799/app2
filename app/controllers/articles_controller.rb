ef2c87cd53fc24d3fffb4ad414781c5e6f8b379f
raohamza <rao.hamza@devbox.co>
2021-12-01 07:19:45 +0000

    byebug addded

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,56 @@
+class ArticlesController < ApplicationController
+    def show
+
+        @article = Article.find(params[:id])
+
+    end
+
+
+    def index
+        @articles=Article.all     
+    end
+    def new
+        @article=Article.new
+    end
+    def edit
+        
+        @article = Article.find(params[:id])
+    end   
+    def update 
+        
+        byebeg
+        show
+
+
+
+
+    end
+
+
+
+
+
+    def create
+         
+
+        @article = Article.new(params.require(:article).permit(:text, :description))
+
+         if @article.save
+            flash[:notice] = "Article was created successfully."
+            redirect_to  @article
+
+         else
+            render 'new'
+         end
+               
+          
+
+    end 
+
+
+
+
+
+
+
+end
