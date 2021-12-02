d3ceab0bab7fb1a32045bcadd26247f68b1b8aee
raohamza <rao.hamza@devbox.co>
2021-12-02 04:26:22 +0000

    modify

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,8 @@
 class ArticlesController < ApplicationController
-    def show
+    
+  
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+  def show
 
         @article = Article.find(params[:id])
 
@@ -13,27 +16,27 @@ class ArticlesController < ApplicationController
         @article=Article.new
     end
     def edit
-        
-        @article = Article.find(params[:id])
+      
     end   
     def update 
+      
+        if @article.update(article_params)
+          flash[:notice] = "Article was updated successfully."
+          redirect_to @article
+        else
+          render 'edit'
+        end
         
-        byebeg
-        show
-
-
-
-
     end
-
-
-
-
+     def destroy
+      
+        redirect_to articles_path
+     end
 
     def create
          
 
-        @article = Article.new(params.require(:article).permit(:text, :description))
+        @article = Article.new(article_params)
 
          if @article.save
             flash[:notice] = "Article was created successfully."
@@ -47,7 +50,15 @@ class ArticlesController < ApplicationController
 
     end 
 
+private 
 
+def set_article
+  @article = Article.find(params[:id])
+end
+
+def article_params
+  params.require(:article).permit(:title, :description)
+end
 
 
 
